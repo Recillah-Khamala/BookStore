a03ecc524d08927d3f6a0d1d13a4c7055251c275
recillah khamala <recillahk@gmail.com>
2022-11-05 19:52:39 +0300

    Created and populated the Form.jsx file

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { TbMinusVertical } from 'react-icons/tb';
+import React from "react";
+import { TbMinusVertical } from "react-icons/tb";
 
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 function Book(props) {
   const { title, author } = props;
@@ -12,27 +12,22 @@ function Book(props) {
         <div className="book-content">
           <div className="book-info">
             <h4 className="book-category">Genre</h4>
-            <h2 className="book-title">{ title }</h2>
-            <h6 className="book-author">{ author }</h6>
+            <h2 className="book-title">{title}</h2>
+            <h6 className="book-author">{author}</h6>
             <div className="action-buttons">
-              <button
-                className="button-outline"
-                type="button"
-              >
+              <button className="button-outline" type="button">
                 Comments
               </button>
-              <div><TbMinusVertical /></div>
-              <button
-                className="button-outline"
-                type="button"
-              >
+              <div>
+                <TbMinusVertical />
+              </div>
+              <button className="button-outline" type="button">
                 Remove
               </button>
-              <div><TbMinusVertical /></div>
-              <button
-                className="button-outline"
-                type="button"
-              >
+              <div>
+                <TbMinusVertical />
+              </div>
+              <button className="button-outline" type="button">
                 Edit
               </button>
             </div>
@@ -46,7 +41,9 @@ function Book(props) {
                 <p className="percentage-complete">64%</p>
                 <p className="completed">Completed</p>
               </div>
-              <div className="progress-divider"><TbMinusVertical /></div>
+              <div className="progress-divider">
+                <TbMinusVertical />
+              </div>
             </div>
             <div className="current-chapter-container">
               <div>
@@ -54,10 +51,7 @@ function Book(props) {
                 <p className="current-chapter">Chapter 17</p>
               </div>
               <div>
-                <button
-                  type="button"
-                  className="primary-button"
-                >
+                <button type="button" className="primary-button">
                   UPDATE PROGRESS
                 </button>
               </div>
