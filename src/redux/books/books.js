8029ac580a5d9b85b33c121b4ef4787dd19df6e4
recillah khamala <recillahk@gmail.com>
2022-11-09 17:14:47 +0300

    Write reducer and export it as default and In case of unknown action - return the current state.

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -11,3 +11,16 @@ const RemoveBook = (book) => ({
   type: removeBook,
   id,
 });
+
+export const bookReducers = (state = initialState, action) => {
+  switch (action.type) {
+    case addBook:
+      return [...state, action.book];
+    case removeBook:
+      return state.filter((book) => book.id === !action.id);
+    default:
+      return state;
+  }
+};
+
+export default { bookReducers, AddBook, RemoveBook}
