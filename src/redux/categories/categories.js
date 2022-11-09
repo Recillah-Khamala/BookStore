611e86e5362d2e1bbcf8fef2cb9595efbc22dd2a
recillah khamala <recillahk@gmail.com>
2022-11-09 17:33:55 +0300

    For the check status action return a string "Under construction".

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,3 +1,12 @@
-export const { checkStatus } = categories.actions
+export const { checkStatus } = categories.actions;
 
-initialState = []
+initialState = [];
+
+const checkReducers = (state = initialState, action) => {
+  switch (action.type) {
+    case checkStatus:
+      return 'under construction';
+    default:
+      return state;
+  }
+};
