211729f3d635bffa77a4e89afe9704b8e4ce6f74
recillah khamala <recillahk@gmail.com>
2022-11-09 16:18:57 +0300

    Define state changes for the actions that you created.

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,3 +1,13 @@
-initialState = []
+export const { addBook, removeBook } = books.actions;
 
-export const { addBook, removeBook } = books.actions
+initialState = [];
+
+const AddBook = (book) => ({
+  type: addBook,
+  book,
+});
+
+const RemoveBook = (book) => ({
+  type: removeBook,
+  id,
+});
