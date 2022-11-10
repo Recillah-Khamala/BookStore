733382c85ad89c2ee545f4bc324560fe7f8b4aa7
recillah khamala <recillahk@gmail.com>
2022-11-10 11:02:48 +0300

    Change the default state in the  books reducer from an empty array to an array with a few books.

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,11 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const bookSlice = createSlice({
-  reducers:{
+  name: 'books',
+  initialState: [
+    {
+      id: uuidv4(),
+      title: 'The Smart parent',
+      aythor: 'Nancy Van Pelt',
+    },
+    {
+      id: uuidv4(),
+      title: 'Choose Your Hard',
+      aythor: 'Recillah Khamala',
+    },
+    {
+      id: uuidv4(),
+      title: 'The hunger games',
+      author: 'Suzanne Collins',
+    },
+    {
+      id: uuidv4(),
+      title: 'Dune',
+      author: 'Frank Herbert',
+    },
+    {
+      id: uuidv4(),
+      title: 'Capital in the Twenty-First Century',
+      author: 'Suzanne Collins',
+    },
+  ],
+
+  reducers: {
     addBook: (state, action) => [...state, action.payload],
-    removeBook: (state, action) => state.filter((book) => book.id === !action.payload)
-  }
-})
+    removeBook: (state, action) =>
+      state.filter((book) => book.id === !action.payload),
+  },
+});
 
 const addBook = 'redux/books/book/addBook';
 const removeBook = 'redux/books/book/removeBook';
